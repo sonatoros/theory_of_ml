b9783bc75d9ea01fc134aac63262eb24c55982ab
Sona <31259731+sonatoros@users.noreply.github.com>
2022-03-14 21:39:22 -0600

    problem 1 part c

---
+++ hw2-grad-desc.py
@@ -0,0 +1,61 @@
+#reference 
+
+
+from numpy import asarray
+from numpy.random import rand
+import math
+import numpy
+
+# objective function
+def objective(w,x,y):
+    L = 0
+    for i in range(len(x)):
+        L +=l(x[i],y[i],w)
+    return L
+
+def l(xi,yi,w):
+    # print(yi,xi,w)
+    return math.log((1+math.exp(-1*yi*xi*w)),2)
+
+# derivative of objective function
+def derivative(w,x,y):
+    Ld = 0
+    for i in range(len(x)):
+        Ld +=ld(x[i],y[i],w)
+    return Ld
+
+def ld(w,xi,yi):
+    x = -1*xi*yi*w
+    return -1*yi*xi*math.exp(x)/(1+math.exp(x))
+
+
+
+# gradient descent algorithm
+def gradient_descent(objective, derivative, n_iter, step_size,x,y):
+	# generate an initial point
+	# w = bounds[:, 0] + rand(len(bounds)) * (bounds[:, 1] - bounds[:, 0])
+    w = -1
+	# run the gradient descent
+    for i in range(n_iter):
+		# calculate gradient
+        gradient = derivative(w,x,y)
+		# take a step
+        w = w - step_size * gradient
+		# evaluate candidate point
+        w_eval = objective(w,x,y)
+
+		# report progress
+        print('>%d f(%s) = %.5f' % (i, w, w_eval))
+    return [w, w_eval]
+
+# define the total iterations
+n_iter = 100
+# define the step size
+step_size = 0.12
+
+x = list(range(-50,51))
+y = numpy.tile([-1,1], 100)
+# perform the gradient descent search
+best, score = gradient_descent(objective, derivative, n_iter, step_size, x,y)
+print('Done!')
+print('f(%s) = %f' % (best, score))
