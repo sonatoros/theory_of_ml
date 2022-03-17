79968d4e0c6736fcda797ac7f6674311f565f259
Sona <31259731+sonatoros@users.noreply.github.com>
2022-03-17 00:39:06 -0600

    part e with inverse calculation

--- gradient_descent_matrix.py
+++ gradient_descent_matrix.py
@@ -10,9 +10,6 @@ def gradient_descent(A, b, T, stepsize):
     for i in range(T):
         f[i] = np.linalg.norm(A.dot(theta)-b)**2   #norm of (Ax-b)^2
         g = 2*np.transpose(A).dot(A.dot(theta) - b)  #gradient
-        # print("gshape", g.shape)
-        k = stepsize*g
-        # print(k.shape)
         theta = theta - stepsize*g
     return theta, f
 
@@ -26,11 +23,13 @@ m=2*n
 A = np.random.uniform(-1,1, (m,n))
 x_star = np.random.uniform(-1,1,(n,1))
 eta = np.random.normal(0, 0.5, (m,1))
-print(eta.shape)
 b = A.dot(x_star) + eta
-print(b.shape)
-# print(b+eta)
 
 T = 50 #number of steps
 theta, f = gradient_descent(A,b, T, 1/10)
 print(theta, f)
+
+#part e inverse
+
+x_st = np.linalg.inv((np.transpose(A).dot(A))).dot(np.transpose(A)).dot(b)
+print("X", x_st)
